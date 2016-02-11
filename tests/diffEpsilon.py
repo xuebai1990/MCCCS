b0fe99f31663273a7c3bdb8c3474545c586f0572
Rob DeJaco <dejac001@umn.edu>
2016-02-10 22:36:06 -0600

    Adapt testing script to new info written in files
    Output files now output things like github branch,
    github head, compiler, etc. The test suite should
    ignore this now

--- tests/diffEpsilon.py
+++ tests/diffEpsilon.py
@@ -26,7 +26,8 @@ def diffLine(str1,str2,incomparable_val=1):
     return distance
 
 def getLine(file,lineNr):
-  bannerRe=re.compile(r"Program started|Program ended|Number of processors|Threads per processor")
+  bannerRe=re.compile(r"Program started|Program ended|Number of processors|Threads per processor|"
+                        +r"MCCCS topmon|Commit hash|Build on host|Preprocessor definitions|compiler")
   line=file.readline()
   if line!="":
     lineNr=lineNr+1
